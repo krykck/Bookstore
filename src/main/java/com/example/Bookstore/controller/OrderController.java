da0e333917e28e1d8b51c49835aa77169aa01589
koray.kocak <koray.kocak@bites.com.tr>
2023-07-03 18:05:48 +0300

    Made some changes in OrderController

--- src/main/java/com/example/Bookstore/controller/OrderController.java
+++ src/main/java/com/example/Bookstore/controller/OrderController.java
@@ -1,10 +1,13 @@
 package com.example.Bookstore.controller;
 
+import com.example.Bookstore.model.Book;
 import com.example.Bookstore.model.Order;
+import com.example.Bookstore.service.BookService;
 import com.example.Bookstore.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.Optional;
 
 @RestController
@@ -14,6 +17,9 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    BookService bookService;
+
     @GetMapping("/id")
     public Optional<Order> getOrder(@PathVariable Integer id) {
         return orderService.getOrder(id);
@@ -22,6 +28,13 @@ public class OrderController {
     @PostMapping("")
     public Order postOrder(@RequestBody Order order) {
         Double totalPrice = order.calculateTotalAmount(order);
+        HashSet<Book> books = (HashSet<Book>) order.getBooks();
+        HashSet<Book> temp = new HashSet<Book>();
+        for (Book b: books) {
+            Optional<Book> toAdd = bookService.getBook(b.getId());
+            toAdd.ifPresent(temp::add);
+        }
+        order.setBooks(temp);
         order.setTotalPrice(totalPrice);
         return orderService.createOrder(order);
     }
