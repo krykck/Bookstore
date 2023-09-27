cd1abb06a2298b7f011b1965962bf28fcaa9e4e1
Koray Kocak <koray__kocak@hotmail.com>
2023-09-27 06:53:32 +0300

    asdasdsadad

--- src/main/java/com/example/Bookstore/config/ApplicationConfig.java
+++ src/main/java/com/example/Bookstore/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.example.Bookstore.config;
 
 import com.example.Bookstore.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -11,7 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public class ApplicationConfig {
 
     private final UserRepository repository;
-
+    @Bean
     public UserDetailsService userDetailsService() {
         return username -> repository.findUserByEmail(username)
                 .orElseThrow(() -> new RuntimeException("User not found"));
